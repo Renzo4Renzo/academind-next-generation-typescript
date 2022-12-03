ef5fdca3718e6bfc576575961b57c8d47df5ae15
Renzo Diaz <renzodiaz554@gmail.com>
2022-12-03 12:41:53 -0500

    Fixing typo in file

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,1 @@
-// const wrestlers = ["TripleH", "Undertaker"];
-
-// // const wrestler1 = wrestlers[0];
-// // const wrestler2 = wrestlers[1];
-
-// const [wrestler1, wrestler2, ...remainingWrestlers] = wrestlers;
-
-// console.log(wrestlers, wrestler1, wrestler2, remainingWrestlers);
-
-// const angel = {
-//   title: "Dark Angel",
-//   years: 30,
-// };
-
-// const { title: subject, years } = angel;
-// console.log(subject, years, angel);
+// Paste the code from any other file here to execute it
